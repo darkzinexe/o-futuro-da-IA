be0fd1d597eb4c1816dc004530960089c0a7c82a
darkzinexe <145065311+darkzinexe@users.noreply.github.com>
2025-08-28 17:14:04 -0300

    Primeira questão em js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        "1. Qual dos impactos a IA vai causar no mercado futuro?"
+        alternativas["Substituir totalmente empregos humanos.", "Criar novos tipos de empregos."]
+    }
+]
