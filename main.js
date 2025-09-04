656631a62c6822638530a5b5277a60af2c981145
darkzinexe <145065311+darkzinexe@users.noreply.github.com>
2025-09-04 16:54:40 -0300

    perguntas

--- main.js
+++ main.js
@@ -7,54 +7,54 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado:
-        "1. Qual dos impactos a IA vai causar no mercado futuro?"
+        "1. Qual dos impactos a IA vai causar no mercado futuro?",
 
-        alternativas[
+        alternativas:[
             "Substituir totalmente empregos humanos.",
              "Criar novos tipos de empregos."
         ]
     },
     {
         enunciado:
-        "2. Qual dessas áreas é considerada uma das maiores tendências futuras da IA nos próximos anos?"
+        "2. Qual dessas áreas é considerada uma das maiores tendências futuras da IA nos próximos anos?",
 
-        alternativas[
+        alternativas:[
             "Realidade aumentada aplicada apenas a jogos",
              "Modelos multimodais que combinam texto, imagem, som e vídeo"
         ]
     },
     {
         enunciado:
-        "3. O que se espera que aumente com o avanço da IA generativa nos próximos anos?"
+        "3. O que se espera que aumente com o avanço da IA generativa nos próximos anos?",
 
-        alternativas[
+        alternativas:[
             "A criação automática de conteúdo em diversas mídias (texto, imagem, vídeo, áudio)",
              "A diminuição do uso de redes neurais profundas"
         ]
     },
     {
         enunciado:
-        "4. Em relação à ética na IA, qual tendência está ganhando mais atenção globalmente?"
+        "4. Em relação à ética na IA, qual tendência está ganhando mais atenção globalmente?",
 
-        alternativas[
+        alternativas:[
             "Tornar todos os modelos de IA de código fechado obrigatoriamente",
              "Desenvolvimento de regulamentações e IA responsável"
         ]
     },
     {
         enunciado:
-        "5. Qual é uma tendência no uso de IA em ambientes corporativos?"
+        "5. Qual é uma tendência no uso de IA em ambientes corporativos?",
 
-        alternativas[
+        alternativas:[
             "Automação inteligente de processos (como RH, jurídico, finanças)",
              "Exclusivamente uso da IA em marketing e vendas"
         ]
     },
     {
         enunciado:
-        "6. O que se espera sobre os modelos de linguagem no futuro próximo?"
+        "6. O que se espera sobre os modelos de linguagem no futuro próximo?",
 
-        alternativas[
+        alternativas:[
             "Serão mais eficientes, personalizados e com menor consumo de energia",
              "Serão sempre maiores, mais lentos e inacessíveis ao público geral"
         ]
