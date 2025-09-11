cb836cb7a9ec49eea71f71062a15e222bfc04eb3
darkzinexe <145065311+darkzinexe@users.noreply.github.com>
2025-09-11 16:55:06 -0300

    js

--- main.js
+++ main.js
@@ -67,6 +67,15 @@ let perguntaAtual;
 function mostrarPergunta(){
     perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 mostrarPergunta();
